cdcbc1daa328f0179ba24a0d528c88234f5f6cbe
EshanAnjum <eshananjum1@gmail.com>
2025-08-12 12:53:35 +0500

    Done

---
+++ Frontend/my-project/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="bg-blue-900 text-white item-center justify-center">
+        <hr className=" border-black" />
+        <h1 className="text-center py-3 text-sm">
+          Copyright © 2025 - All right reserved
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
